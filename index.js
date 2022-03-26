16e9c4d227961ade3471f65da5efabfb1dd472ba
HRISHIKESH BHAGAT <35609345+HrishikeshBhagat@users.noreply.github.com>
2022-03-26 15:53:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function msg(){ 
+    alert("Thanks for your kind feedback & visiting LeafyFood");  
+    }
+    document.getElementById('menuhead1').click (()=>{
+    console.log('you have clicked the menu heading');
+    location.href = 'https://www.youtube.com/'
+});
